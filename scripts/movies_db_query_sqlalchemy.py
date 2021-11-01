51463a537f86d1fd3cef186db17f22f026109203
Abhishek Bhatia <abhi@mac.local>
2021-11-01 18:47:23 +0530

    add sqlalchemy script for movies database

---
+++ scripts/movies_db_query_sqlalchemy.py
@@ -0,0 +1,48 @@
+import sqlalchemy as db
+from pathlib import Path
+
+DATABASE_PATH = Path(__file__).parents[1].absolute().joinpath('data/movies.db')
+
+engine = db.create_engine(f'sqlite:///{DATABASE_PATH}')
+connection = engine.connect()
+
+metadata = db.MetaData()
+movies = db.Table('movies', metadata, autoload=True, autoload_with=engine)
+
+print('Selecting all records from movies table...')
+query = db.select(movies)
+result_proxy = connection.execute(query)
+result_set = result_proxy.fetchall()
+print(result_set, '\n')
+
+director = 'Steven Spielberg'
+print(f'Selecting records where director is {director}...')
+query = db.select(movies).where(movies.c.director == director)
+result_proxy = connection.execute(query)
+result_set = result_proxy.fetchall()
+print(result_set, '\n')
+
+print('Inserting a Psycho movie into the movies table...')
+query = movies.insert().values(title='Psycho',
+                               director='Alfred Hitchcock',
+                               year=1960)
+connection.execute(query)
+print('Done!', '\n')
+
+print('Selecting all records from movies table again...')
+query = db.select(movies)
+result_proxy = connection.execute(query)
+result_set = result_proxy.fetchall()
+print(result_set, '\n')
+
+
+print('Deleting previously inserted record...')
+query = db.delete(movies).where(movies.c.title == 'Psycho')
+connection.execute(query)
+print('Done!', '\n')
+
+print('Selecting all records from movies table once again...')
+query = db.select(movies)
+result_proxy = connection.execute(query)
+result_set = result_proxy.fetchall()
+print(result_set, '\n')
