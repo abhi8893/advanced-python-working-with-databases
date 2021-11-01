7cde6a76cf79afd7b441a6637d9bb64996c0c58a
Abhishek Bhatia <abhi@mac.local>
2021-11-01 20:50:45 +0530

    add sqlalchemy challenge for users.db

--- scripts/sqlalchemy_challenge_users_db.py
+++ scripts/sqlalchemy_challenge_users_db.py
@@ -9,3 +9,40 @@ except FileNotFoundError:
 
 engine = db.create_engine(f'sqlite:///{DATABASE_PATH}')
 connection = engine.connect()
+metadata = db.MetaData()
+
+users = db.Table(
+    'Users', metadata,
+    db.Column('user_id', db.INTEGER, primary_key=True),
+    db.Column('first_name', db.TEXT),
+    db.Column('last_name', db.TEXT),
+    db.Column('email', db.TEXT)
+    )
+
+metadata.create_all(engine)
+
+users_details = [
+    ('Abhishek', 'Bhatia'),
+    ('Seema', 'Bhatia'),
+    ('Dinesh', 'Bhatia'),
+    ('Akshay', 'Bhatia'),
+    ('Divya', 'Bhatia'),
+    ('Anurima', 'Dey')
+]
+
+
+query = users.insert().values([
+    {'first_name': f, 'last_name': l,
+     'email': f'{f.lower()}.{l.lower()}@email.com'}
+    for f, l in users_details
+    ])
+
+connection.execute(query)
+
+query = db.select(users)
+result_proxy = connection.execute(query)
+result_set = result_proxy.fetchall()
+
+print(result_set)
+
+connection.close()
